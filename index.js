c072efc21dfc763253f3ad0064ac53bbff7eaf02
Yuvraj883 <kingyuvi88@gmail.com>
2022-07-05 01:37:59 +0530

    'Copy To Clipboard' button made hidden as long as password is not generated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-
+let displayBtn = false;
 let passwordEl1 = document.querySelector("#password-el1") ;
 let passwordEl2 = document.querySelector("#password-el2") ;
 
@@ -20,6 +20,7 @@ let password2="";
   passwordEl2.textContent = password2; 
   passwordEl1.textcontent = "";
   passwordEl2.textcontent = "";
+  document.querySelector("#btn-container").classList.replace('hidden', 'btn-container')
 }
 
 
